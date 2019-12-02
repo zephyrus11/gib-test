123603fef4190ca103f548d81ec8708c4c05a421
zephyrus11 <37396597+zephyrus11@users.noreply.github.com>
2019-12-02 09:07:28 +0100

    Updated media queries

--- Interview/js/scripts.js
+++ Interview/js/scripts.js
@@ -1,11 +1,10 @@
 $(window).load(function() {
     // Animate loader off screen
-    $(".spinner").fadeOut(1000);;
+    $(".spinner").fadeOut(1000);
 });
 
 function myFunction() {
     var burger = document.getElementById("burgerMenu");
-    var main = document.getElementById("content");
     if (burger.style.width === "250px") {
       burger.style.width = "0";
       burger.style.display = "none";
@@ -14,3 +13,37 @@ function myFunction() {
       burger.style.display = "block";
     }
   }
+function showAll() {
+    var football = document.getElementById("football");
+    var badminton = document.getElementById("badminton");
+    var snooker = document.getElementById("snooker");
+    var rugby = document.getElementById("rugby");
+    football.style.display = "block";
+    badminton.style.display = "block";
+    snooker.style.display = "block";
+    rugby.style.display = "block";
+}
+function showFootball() {
+    football.style.display = "block";
+    badminton.style.display = "none";
+    snooker.style.display = "none";
+    rugby.style.display = "none";
+}
+function showBadminton() {
+    badminton.style.display = "block";
+    football.style.display = "none";
+    snooker.style.display = "none";
+    rugby.style.display = "none";
+}
+function showSnooker() {
+    snooker.style.display = "block";
+    football.style.display = "none";
+    badminton.style.display = "none";
+    rugby.style.display = "none";
+}
+function showRugby() {
+    rugby.style.display = "block";
+    football.style.display = "none";
+    badminton.style.display = "none";
+    snooker.style.display = "none";
+}
